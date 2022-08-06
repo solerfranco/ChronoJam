1cc8155f1817761ad0ea29ab4af76a49019a7ac5
Franco Soler <francosolerlanderreche@gmail.com>
2022-08-06 18:32:05 -0300

    Cloud spawner

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
-    public Sprite[] variants;
-    private SpriteRenderer spriteRend;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        spriteRend = GetComponent<SpriteRenderer>();
-        spriteRend.sprite = variants[Random.Range(0, variants.Length)];
     }
 
     void FixedUpdate()
--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -7,17 +7,32 @@ public class ObjectSpawner : MonoBehaviour
     public GameObject prefab;
     public float minTime;
     public float maxTime;
+    public bool move;
+    private SpriteRenderer spriteRend;
+    public Sprite[] variants;
+    private int lastRandom = 0;
+    private int currentRandom = 0;
 
     private void Start()
     {
-        LeanTween.moveY(gameObject, 5, 0.6f).setEaseInOutQuad().setLoopPingPong(-1);
+        if(move)LeanTween.moveY(gameObject, 5, 0.6f).setEaseInOutQuad().setLoopPingPong(-1);
         StartCoroutine(SpawnObject());
     }
 
     private IEnumerator SpawnObject()
     {
         yield return new WaitForSeconds(Random.Range(minTime, maxTime));
-        Instantiate(prefab, transform.position, Quaternion.identity, null);
+        GameObject objectToSpawn = Instantiate(prefab, transform.position, Quaternion.identity, null);
+        if (variants.Length > 0)
+        {
+            spriteRend = objectToSpawn.GetComponent<SpriteRenderer>(); do
+            {
+                currentRandom = Random.Range(0, variants.Length);
+            }
+            while (lastRandom == currentRandom);
+            spriteRend.sprite = variants[currentRandom];
+            lastRandom = currentRandom;
+        }
         StartCoroutine(SpawnObject());
     }
 }
